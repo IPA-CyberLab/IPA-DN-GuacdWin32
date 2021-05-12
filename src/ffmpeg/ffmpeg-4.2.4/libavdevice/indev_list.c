bb6eafca98b2f95756abe89644306487fd91b81e
SE Buildsan <da.buildsan@softether.co.jp>
2021-05-13 00:30:01 +0900

    configured

---
+++ src/ffmpeg/ffmpeg-4.2.4/libavdevice/indev_list.c
@@ -0,0 +1,7 @@
+static const AVInputFormat * const indev_list[] = {
+    &ff_gdigrab_demuxer,
+    &ff_lavfi_demuxer,
+    &ff_oss_demuxer,
+    &ff_vfwcap_demuxer,
+    &ff_xcbgrab_demuxer,
+    NULL };
---
+++ src/ffmpeg/ffmpeg-4.2.4/libavdevice/outdev_list.c
@@ -0,0 +1,3 @@
+static const AVOutputFormat * const outdev_list[] = {
+    &ff_oss_muxer,
+    NULL };
