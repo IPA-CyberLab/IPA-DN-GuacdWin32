cac4fd5a5cd79bafebe3384830b370d1e93802af
SE Buildsan <da.buildsan@softether.co.jp>
2021-05-25 17:45:23 +0900

    CopyRelease.cmd

---
+++ src/guacd/guacamole-server-1.3.0/CopyRelease.cmd
@@ -0,0 +1,76 @@
+cd /d %~dp0
+
+rmdir /q /s out\
+mkdir out\
+
+copy ThinWebGateway.exe out\
+copy C:\cygwin\bin\cygX11-6.dll out\
+copy C:\cygwin\bin\cygcairo-2.dll out\
+copy C:\cygwin\bin\cygcrypto-1.1.dll out\
+copy C:\cygwin\bin\cygwin1.dll out\
+copy C:\cygwin\bin\cyggobject-2.0-0.dll out\
+copy C:\cygwin\bin\cygjpeg-8.dll out\
+copy C:\cygwin\bin\cygpango-1.0-0.dll out\
+copy C:\cygwin\bin\cygpangocairo-1.0-0.dll out\
+copy C:\cygwin\bin\cygpng16-16.dll out\
+copy C:\cygwin\bin\cygpulse-0.dll out\
+copy C:\cygwin\bin\cygssh2-1.dll out\
+copy C:\cygwin\bin\cygssl-1.1.dll out\
+copy C:\cygwin\bin\cygvncclient-1.dll out\
+copy C:\cygwin\bin\cygwebp-7.dll out\
+copy C:\cygwin\bin\cyggcc_s-1.dll out\
+copy C:\cygwin\bin\cygffi-6.dll out\
+copy C:\cygwin\bin\cygthai-0.dll out\
+copy C:\cygwin\bin\cygpulsecommon-11.1.dll out\
+copy C:\cygwin\bin\cygdbus-1-3.dll out\
+copy C:\cygwin\bin\cygcrypto-1.0.0.dll out\
+copy C:\cygwin\bin\cyggcrypt-20.dll out\
+copy C:\cygwin\bin\cyggnutls-30.dll out\
+copy C:\cygwin\bin\cygdatrie-1.dll out\
+copy C:\cygwin\bin\cygasyncns-0.dll out\
+copy C:\cygwin\bin\cygsndfile-1.dll out\
+copy C:\cygwin\bin\cygxcb-1.dll out\
+copy C:\cygwin\bin\cygglib-2.0-0.dll out\
+copy C:\cygwin\bin\cygpangoft2-1.0-0.dll out\
+copy C:\cygwin\bin\cygfontconfig-1.dll out\
+copy C:\cygwin\bin\cygfreetype-6.dll out\
+copy C:\cygwin\bin\cyguuid-1.dll out\
+copy C:\cygwin\bin\cygbrotlidec-1.dll out\
+copy C:\cygwin\bin\cygpcre-1.dll out\
+copy C:\cygwin\bin\cygharfbuzz-0.dll out\
+copy C:\cygwin\bin\cygz.dll out\
+copy C:\cygwin\bin\cygpixman-1-0.dll out\
+copy C:\cygwin\bin\cygxcb-render-0.dll out\
+copy C:\cygwin\bin\cygxcb-shm-0.dll out\
+copy C:\cygwin\bin\cygXrender-1.dll out\
+copy C:\cygwin\bin\cygXext-6.dll out\
+copy C:\cygwin\bin\cygiconv-2.dll out\
+copy C:\cygwin\bin\cygXau-6.dll out\
+copy C:\cygwin\bin\cygXdmcp-6.dll out\
+copy C:\cygwin\bin\cygexpat-1.dll out\
+copy C:\cygwin\bin\cygintl-8.dll out\
+copy C:\cygwin\bin\cygbz2-1.dll out\
+copy C:\cygwin\bin\cygwrap-0.dll out\
+copy C:\cygwin\bin\cyggpg-error-0.dll out\
+copy C:\cygwin\bin\cyggmp-10.dll out\
+copy C:\cygwin\bin\cyghogweed-4.dll out\
+copy C:\cygwin\bin\cygidn2-0.dll out\
+copy C:\cygwin\bin\cygtasn1-6.dll out\
+copy C:\cygwin\bin\cygnettle-6.dll out\
+copy C:\cygwin\bin\cygp11-kit-0.dll out\
+copy C:\cygwin\bin\cygunistring-2.dll out\
+copy C:\cygwin\bin\cygbrotlicommon-1.dll out\
+copy C:\cygwin\bin\cyggraphite2-3.dll out\
+copy C:\cygwin\bin\cygFLAC-8.dll out\
+copy C:\cygwin\bin\cyggsm-1.dll out\
+copy C:\cygwin\bin\cygogg-0.dll out\
+copy C:\cygwin\bin\cygvorbis-0.dll out\
+copy C:\cygwin\bin\cygvorbisenc-2.dll out\
+copy "C:\cygwin\bin\cygstdc++-6.dll" out\
+
+
+
+
+
+
+
