c5a0ee3e1b54b5f17373c7541725e7501e01d467
SE Buildsan <da.buildsan@softether.co.jp>
2021-07-11 01:35:22 +0900

    Revert "一度 色々非同期ソケットをやめてみた"
    This reverts commit 79c028cb58e8e4d8ea152cc2c16cc2169c63a57e.

--- src/guacd/guacamole-server-1.3.0/src/FreeRDP-2.0.0/libfreerdp/core/transport.c
+++ src/guacd/guacamole-server-1.3.0/src/FreeRDP-2.0.0/libfreerdp/core/transport.c
@@ -227,7 +227,6 @@ wStream* transport_send_stream_init(rdpTransport* transport, int size)
 
 BOOL transport_attach(rdpTransport* transport, int sockfd)
 {
-	WHERE;
 	BIO* socketBio = NULL;
 	BIO* bufferedBio;
 	socketBio = BIO_new(BIO_s_simple_socket());
@@ -236,13 +235,13 @@ BOOL transport_attach(rdpTransport* transport, int sockfd)
 		goto fail;
 
 	BIO_set_fd(socketBio, sockfd, BIO_CLOSE);
-	//bufferedBio = BIO_new(BIO_s_buffered_socket());
+	bufferedBio = BIO_new(BIO_s_buffered_socket());
 
-	//if (!bufferedBio)
-	//	goto fail;
+	if (!bufferedBio)
+		goto fail;
 
-	//bufferedBio = BIO_push(bufferedBio, socketBio);
-	transport->frontBio = socketBio;
+	bufferedBio = BIO_push(bufferedBio, socketBio);
+	transport->frontBio = bufferedBio;
 	return TRUE;
 fail:
 
@@ -553,8 +552,6 @@ static SSIZE_T transport_read_layer(rdpTransport* transport, BYTE* data, size_t 
 	SSIZE_T read = 0;
 	rdpRdp* rdp = transport->context->rdp;
 
-	//WHERE;
-
 	if (!transport->frontBio || (bytes > SSIZE_MAX))
 	{
 		transport->layer = TRANSPORT_LAYER_CLOSED;
@@ -631,11 +628,7 @@ static SSIZE_T transport_read_layer_bytes(rdpTransport* transport, wStream* s, s
 	if (toRead > SSIZE_MAX)
 		return 0;
 
-	EnterCriticalSection(&(transport->WriteLock));
-
 	status = transport_read_layer(transport, Stream_Pointer(s), toRead);
-
-	LeaveCriticalSection(&(transport->WriteLock));
 
 	if (status <= 0)
 		return status;
@@ -804,8 +797,6 @@ int transport_write(rdpTransport* transport, wStream* s)
 	int status = -1;
 	int writtenlength = 0;
 	rdpRdp* rdp = transport->context->rdp;
-
-	//WHERE;
 
 	if (!s)
 		return -1;
